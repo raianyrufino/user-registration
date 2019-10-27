13040f94dd74b5ed342f9ee7783867ee560b8073
raianyrufino <raiany.paz@ccc.ufcg.edu.br>
2019-10-27 12:56:47 -0300

    updateField and renderForm

--- frontend/src/components/templates/Footer.jsx
+++ frontend/src/components/templates/Footer.jsx
@@ -1,12 +1,10 @@
-import "./Footer.css"
+import './Footer.css'
 import React from 'react'
 
-
-export default props =>{
+export default props =>
     <footer className="footer">
         <span>
-            Desenvolvido com <i className="fa fa-heart text-danger"></i> por 
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por
             <strong> Cod<span className="text-danger">3</span>r</strong>
         </span>
     </footer>
-}
--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -2,17 +2,19 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 import './App.css'
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 
 import Logo from '../components/templates/logo'
 import Nav from '../components/templates/Nav'
-import Main from '../components/templates/Main'
+import Routes from './routes'
 import Footer from '../components/templates/Footer'
 
 export default props =>
-    <div className="app">
-        <Logo />
-        <Main icone="home" title="Início"
-            subtitle = "Segundo Projeto do capítulo"/>
-        <Nav />
-        <Footer />
-    </div>
+    <BrowserRouter>
+        <div className="app">
+            <Logo />
+            <Nav />
+            <Routes />
+            <Footer />
+        </div>
+    </BrowserRouter>
